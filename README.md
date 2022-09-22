c55d7a27e0aa545c5eebed1c3fda775e29ae5f12
xavier2001-sudo <xavierp20.aps1b@gmail.com>
2022-09-22 09:16:11 +0300

    importation

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# HACKATON-IFM
-Application Web qui sert d'annuaire des artisans malgaches pour faciliter le contact avec les clients et dans laquelle ils peuvent vendre leurs œuvres directement dans leur propre galerie.
+# ifm - P4H hackathon => Novembre numérique
+
+Web application that serves as a directory of Malagasy artisans to facilitate contact with customers and in which they can sell their works directly in their own gallery.
+
+# Bon courage mes amis. Jésus sera toujours avec Nous et nous aurons toujours confiance en LUI.
---
+++ views/includes/header.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inscription</title>
+    <link rel="shortcut icon" href="../../assets/image/logo/logo.ico" type="image/x-icon">
+    <link rel="stylesheet" href="../../assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../../assets/css/css_additionnels.css">
+
+
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;900&display=swap" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.6.2/animate.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="../assets/css/info-inscription.css"> 
+</head>
+<body >
+<header>
+    <div class="couleur_header">
+        <div class="container-fluid">
+        <nav class="navbar fixed-top navbar-expand-lg navbar-dark p-md-3">
+     <div class="container">
+     <img class="logo_ArtiZone shadow" src="../assets/image/logo/logobg.png" alt="ArtiZone'logo" width="120" height="70">         <button
+            type="button"
+            class="navbar-toggler"
+            data-bs-target="navbarNav"
+            data-bs-toggler="collapse"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-lable="Toggle Navbar"
+         >
+             <span class="navbar-toggler-icon"></span>
+         </button>
+
+         <div class="collapse navbar-collapse" id="navbarNav">
+             <div class="mx-auto"></div>
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a href="#" class="nav-link text-white"> <span style="color:green; font-size:15px; font-weight:bold">Accueil</span> </a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link text-white" ><span style="color:green; font-size:15px; font-weight:bold">Gallerie</span></a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link text-white"><span style="color:green; font-size:15px; font-weight:bold">Catégories</span></a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link text-white"><span style="color:green; font-size:15px; font-weight:bold">Artisans</span></a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link text-white"><span style="color:green; font-size:15px; font-weight:bold">Se connecter</span></a>
+                </li>
+            </ul>
+         </div>
+     </div>   
+    </nav>
+        </div>
+    </div>
+</header>
